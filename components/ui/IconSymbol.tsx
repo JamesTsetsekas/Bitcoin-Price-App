38a5c4369f7b3c2c5c584113c1ef78632c91ad13
James Tsetsekas <jtsetsekas@gmail.com>
2025-07-22 13:42:39 -0400

    Add mappings for 'bitcoinsign.circle.fill' and 'chart.line.uptrend.xyaxis' to IconSymbol

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -18,6 +18,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'bitcoinsign.circle.fill': 'monetization-on',
+  'chart.line.uptrend.xyaxis': 'trending-up',
 } as IconMapping;
 
 /**
